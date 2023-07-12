227f510ddf39b8eea87a5b9c58bc4ed718d0a107
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-07-12 10:29:04 -0700

    Remove unnecessary a tag

--- components/SitePicker/Character.js
+++ components/SitePicker/Character.js
@@ -3,9 +3,9 @@ import Image from 'next/image'
 
 const Character = ({ id, name, image, selected, disabled, ...rest }) => {
 	return (
-		<a className={`character ${!!selected && 'active'} ${!!disabled && 'disabled'}`} data-name={id} rel={name} {...rest}>
+		<div className={`character ${!!selected && 'active'} ${!!disabled && 'disabled'}`} data-name={id} rel={name} {...rest}>
 			<Image src={image} alt={name} width={200} height={200} draggable="false" />
-		</a>
+		</div>
 	)
 }
 
