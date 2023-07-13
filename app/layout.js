57d291f956231db24eb7598aa3bdc403d6cab7da
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-07-12 20:54:46 -0700

    Remove normalize

--- app/layout.js
+++ app/layout.js
@@ -21,9 +21,6 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en" style={{ fontFamily: openSans.style.fontFamily }}>
 			<head>
-				{/* Normalize */}
-				<link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" rel="stylesheet" defer />
-
 				{/* <!-- FontAwesome Icons --> */}
 				<Script src="https://kit.fontawesome.com/d7ccc5bb1a.js" strategy="afterInteractive" crossOrigin="anonymous" />
 			</head>
