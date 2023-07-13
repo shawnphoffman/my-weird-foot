e6b3e6220013db0f869bf47eb7fb69d152e01879
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-07-13 09:58:18 -0700

    Clean next js config

--- next.config.js
+++ next.config.js
@@ -5,23 +5,6 @@ const nextConfig = {
 	experimental: {
 		appDir: true,
 	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 's3-us-west-2.amazonaws.com',
-			},
-		],
-	},
-	webpack(config) {
-		const fileLoaderRule = config.module.rules.find(rule => rule.test?.test?.('.svg'))
-		config.module.rules.push({
-			test: /\.svg$/i,
-			use: ['@svgr/webpack'],
-		})
-		fileLoaderRule.exclude = /\.svg$/i
-		return config
-	},
 }
 
 module.exports = nextConfig
