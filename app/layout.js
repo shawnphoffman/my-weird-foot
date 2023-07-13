ae965b308bc12f5460f778a3ee52702ff24d4344
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-07-12 20:05:59 -0700

    Font optimization

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,13 @@
 import 'styles/globals.css'
 
+import { Open_Sans } from 'next/font/google'
 import Script from 'next/script'
+
+const openSans = Open_Sans({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
+		<html lang="en" style={{ fontFamily: openSans.style.fontFamily }}>
 			<head>
 				{/* Normalize */}
 				<link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" rel="stylesheet" defer />
