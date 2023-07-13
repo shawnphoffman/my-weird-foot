2679beaf8f434d04a245a5b74e717dbecde69b5a
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-07-12 20:22:11 -0700

    Meta images

---
+++ app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+	return [
+		{
+			url: 'https://myweirdfoot.com',
+			lastModified: new Date(),
+		},
+		{
+			url: 'https://myweirdfoot.com/home',
+			lastModified: new Date(),
+		},
+		{
+			url: 'https://myweirdfoot.com/listen-now',
+			lastModified: new Date(),
+		},
+	]
+}
